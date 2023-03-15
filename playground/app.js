11c9a018deda0f5155ef76827762280e99fc5b96
Silvio Delgado <silviomdelgado@gmail.com>
2023-03-15 19:16:52 -0300

    Update playground

--- playground/app.js
+++ playground/app.js
@@ -9,17 +9,23 @@
 })(typeof global !== "undefined" ? global : this.window || this.global, function (root) {
     'use strict';
 
-    var show_about = function () {
+    const show_about = function () {
         alert('This is the application "About".\n\nCopyright ©2018-2019 Interart');
     }
 
-    var show_number = function (num) {
+    const show_number = function (num) {
         alert('Number: ' + num);
     }
 
     Router
         .add(/about/, show_about)
         .add(/number:([0-9]+)/, show_number)
+        .beforeAll(() => {
+            console.log('Run before all routes!')
+        })
+        .afterAll(() => {
+            console.log('Run after all routes!')
+        })
         .apply()
         .start();
 });
